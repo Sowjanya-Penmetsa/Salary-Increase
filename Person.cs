b2d597ad8dea810a3e38000a60c08d135e0d09f2
unknown <penmetsa.sowjanya30@gmail.com>
2020-05-19 15:44:48 +0200

    first commit

---
+++ Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Salary_Increase
+{
+    class Person
+    {
+        public Person(string firstname, string lastname, int age,decimal salary)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        private string firstname;
+        private string lastname;
+        private int age;
+        private decimal salary;
+
+        public string Firstname
+        {
+            get
+            {
+                return this.firstname;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return this.lastname;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+         if(this.age>30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+         else
+            {
+                this.salary += this.salary * percentage / 200;
+
+            }
+        }
+
+       public override string ToString()
+        {
+            return $"{this.firstname}  {this.lastname} receives  {this.salary} dollars";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Salary_Increase
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number of lines");
+
+            var lines = Convert.ToInt32(Console.ReadLine());
+
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+
+            {
+
+                var cmdArgs = Console.ReadLine().Split();
+
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+
+            }
+
+            Console.WriteLine("enter bonus");
+
+            var bonus = decimal.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.ReadLine();
+
+        }
+    }
+}
